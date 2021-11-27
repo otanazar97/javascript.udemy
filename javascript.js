3029a7e836e8c9eb53be75cb10053fa9dd524469
Otanazar97 <Otanazar98@yandex.ru>
2021-11-27 19:05:36 +0500

    first commit

---
+++ javascript.js
@@ -0,0 +1,4 @@
+function work () {
+    let counter = 0;
+}
+
---
+++ js/script.js
@@ -0,0 +1,26 @@
+"use sctrict";
+
+// console.log(-4/0);
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false 
+
+// };
+
+// console.log(obj.name);
+// console.log(obj["name"]);
+
+// alert("hello world");
+
+// const answer = prompt("Вам есть 18?","18");
+// console.log(typeof(answer));
+  
+const answers = [];
+
+// answers[0] = prompt('Ismingizni yozing','');
+// answers[1] = prompt('Familiyangizni yozing','');
+// answers[2] = prompt('Yoshingizni yozing','');
+
+document.write(answers);
