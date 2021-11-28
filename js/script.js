7cc1b682b8e671f801971239d61b4f9519d2af2b
Otanazar97 <Otanazar98@yandex.ru>
2021-11-28 09:54:33 +0500

    javascript add

--- js/script.js
+++ js/script.js
@@ -17,10 +17,41 @@
 // const answer = prompt("Вам есть 18?","18");
 // console.log(typeof(answer));
   
-const answers = [];
+// const answers = [];
 
-// answers[0] = prompt('Ismingizni yozing','');
-// answers[1] = prompt('Familiyangizni yozing','');
-// answers[2] = prompt('Yoshingizni yozing','');
+// // answers[0] = prompt('Ismingizni yozing','');
+// // answers[1] = prompt('Familiyangizni yozing','');
+// // answers[2] = prompt('Yoshingizni yozing','');
 
-document.write(answers);
+// document.write(answers);
+
+// let incr = 10,
+//     decr = 10;
+
+
+// incr ++ ;
+// decr -- ;
+
+// console.log(incr);
+// console.log(decr);
+
+
+const numberOfFilms = +prompt('Siz tomosha qilgan filimlaringiz soni?','');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat: false
+};
+const a = prompt('Oxirgi korgan filmingiz?',''),
+    b = prompt('U filmni qanday baholaysiz?',''),
+    c = prompt('Oxirgi korgan filmingiz?',''),
+    d = prompt('U filmni qanday baholaysiz?','');
+    
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
